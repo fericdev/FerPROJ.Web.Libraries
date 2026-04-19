f1c930346179f531f6e26c4a216be350c8e6f03b
Feric Decenan <feric.decenan.git@gmail.com>
2026-04-19 19:05:32 +0800

    yes

--- FerPROJ.Web.Libraries/BaseModels/BaseModel.cs
+++ FerPROJ.Web.Libraries/BaseModels/BaseModel.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using FerPROJ.Web.Libraries.BaseDataHelper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FerPROJ.Web.Libraries.BaseModels {
     public abstract class BaseModel : ModelValidator {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public DateTime DateCreated { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
         public DateTime? DateModified { get; set; } = null;
         public DateTime? DateDeleted { get; set; } = null;
         public Guid CreatedById { get; set; }
         public Guid? ModifiedById { get; set; } = null;
-        public string Status { get; set; }
+        public string Status { get; set; } = SystemConstants.ACTIVE_STATUS;
     }
 }
